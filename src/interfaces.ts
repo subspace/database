11eec5effe6e5f8c7eaf907f0d99d3ae1822a0f7
Jeremiah Wagstaff <jeremiah.wagstaff@gmail.com>
2018-10-18 22:54:43 -0700

    connected to core db txs

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,3 +1,21 @@
+export interface IRecord {
+  kind: 'mutable' | 'immutable'
+  key: string,
+  value: IValue
+}
+
+export interface IImmutableRecord extends IRecord {
+  kind: 'immutable'
+  value: IImmutableValue
+}
+
+export interface IMutableRecord extends IRecord {
+  kind: 'mutable'
+  value: IMutableValue
+}
+
+export type Record = IImmutableRecord | IMutableRecord
+
 export interface IValue {
   version: number   // subspace database encoding version of this record
   encoding: string  // the value encoding for the content
@@ -6,31 +24,19 @@ export interface IValue {
   owner: string     // the node_id who created the record
   timestamp: number // unix timestamp when created or revised
   size: number      // size of the full record
-  contract: string  // the data contract the record is stored against
+  contract: string  // the id of the data contract this record is assigned to
+  contractSignature: string 
 }
 
 export interface IImmutableValue extends IValue {
 }
 
 export interface IMutableValue extends IValue {
-  pubkey: string    // public key of this record
-  privkey: string   // sym encrypted private key of this record
+  publicKey: string    // public key of this record
+  privateKey: string   // sym encrypted private key of this record
   contentHash: string      // a hash of the decrypted content, for authenticity
   revision: number  // sequence number for conflict resolution
-  signature: string // contract or pub key signature of message
-}
-
-export interface IRecord {
-  key: string,
-  value: IValue
-}
-
-export interface IImmutableRecord extends IRecord {
-  value: IImmutableValue
-}
-
-export interface IMutableRecord extends IRecord {
-  value: IMutableValue
+  recordSignature: string
 }
 
 export interface ShardIndex {
@@ -51,4 +57,36 @@ export interface ShardMap {
   id: string
   hosts: string[]
 }
+
+export interface IContractObject {
+  kind: 'contractObject'
+  id: string
+  owner: string
+  name: string
+  email: string
+  passphrase: string
+  ttl: number
+  replicationFactor: number
+  spaceReserved: number
+  spaceUsed: number
+  createdAt: number
+  updatedAt: number
+  recordIndex: Set<string>
+  publicKey: string
+  privateKey: string
+  privateKeyObject: any
+}
+
+export interface IContractData {
+  kind: 'contractData'
+  publicKey: string
+  clientKey: string
+  createdAt: number
+  ttl: number 
+  spaceReserved: number
+  replicationFactor: number
+}
+
+export type IContract = IContractObject | IContractData
+
 
